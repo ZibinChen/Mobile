2d382c5a0e768270b26bc436bfc2a0629d5efdc7
ZibinChen <zbchen920117@gmail.com>
2026-03-29 21:01:31 +0800

    Update next.config.mjs for output and image settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  }
-}
+  output: 'export',
+  images: { unoptimized: true },
+  // 关键：这里必须改成当前仓库的名字 Mobile
+  basePath: '/Mobile', 
+  assetPrefix: '/Mobile/', 
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+};
 
-export default nextConfig
+export default nextConfig;
